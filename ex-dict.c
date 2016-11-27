1ae4f8aab173bb6cf0ab3b271d7ebacf7ad93869
Caio César <caio.cdcs@gmail.com>
2016-11-27 12:44:38 -0200

    Updated ex-dict to be dynamic

--- ex-dict.c
+++ ex-dict.c
@@ -1,33 +1,72 @@
-/* 
+/*
  * Teste da nova chamada de sistema
- */ 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+const int MAX_SIZE = 100;
+
+int getOptionFromMenu() {
+  int option;
+  printf("Syscall menu \n");
+  printf("0. Exit\n");
+  printf("1. Set  key \n");
+  printf("2. Get key \n");
+  printf("Type your option: ");
+  scanf("%d", &option);
+  return option;
+}
+
+void setNewKey() {
+  char* value = malloc(MAX_SIZE * sizeof(char));
+  int r, key, lifespan;
+
+  printf("\nKey: ");
+  scanf("%d", &key);
+  printf("Value: ");
+  scanf("%s", value);
+  printf("Lifespan (seconds): ");
+  scanf("%d", &lifespan);
+  r = syscall(380, key, value, lifespan);
+  if (r == 0)
+    printf("Success\n");
+  else
+    printf("Error\n");
+  free(value);
+}
+
+void getKey() {
+  char* value = malloc(MAX_SIZE * sizeof(char));
+  int r, key, size;
+  printf("\nKey: ");
+  scanf("%d", &key);
+  printf("Size: ");
+  scanf("%d", &size);
+  r = syscall(381, key, size, value);
+  if (r == 0)
+    printf("Success. Value: %s\n", value);
+  else
+    printf("Error finding the key: %d\n", key);
+  free(value);
+}
+
 int main() {
-  int size = 5;
-  char* ptr2 = malloc(size * sizeof(char));
-  
-  char* ptr = "boo\n";
-  int r = syscall(380, 1, ptr, 100000); 
-  printf("Retorno da chamada de sistema: %d.\n", r);
+  int option;
 
-  r = syscall(381, 1, 4, ptr2); 
-  printf("Retorno da chamada de sistema: %d.\n", r);
-  printf("Retorno da string: %s.\n", ptr2);
+  do {
+    option = getOptionFromMenu();
 
-  free(ptr2);
+    switch(option) {
+      case 1:
+        setNewKey();
+        break;
+      case 2:
+        getKey();
+        break;
+    }
+    printf("\n\n");
+  } while (option != 0);
 
-  ptr = "bla\n";
-  r = syscall(380, 2, ptr, 100000); 
-  printf("Retorno da chamada de sistema: %d.\n", r);
-
-  ptr2 = malloc(size * sizeof(char));
-  r = syscall(381, 2, 4, ptr2); 
-  printf("Retorno da chamada de sistema: %d.\n", r);
-  printf("Retorno da string: %s.\n", ptr2);
-
-  free(ptr2);
-  return r;
+  return 0;
 }
